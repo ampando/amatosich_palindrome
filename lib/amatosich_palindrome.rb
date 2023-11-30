bcdf29540716f5ab693b596432366fe8efa2e701
ampando <amatosich@gmail.com>
2023-11-30 00:27:50 +0000

    Initialize repository

---
+++ lib/amatosich_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "amatosich_palindrome/version"
+
+module AmatosichPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/amatosich_palindrome/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AmatosichPalindrome
+  VERSION = "0.1.0"
+end
