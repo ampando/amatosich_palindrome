8135474a90b579c1a2ac19ec1990be84ad25835c
ampando <amatosich@gmail.com>
2023-11-30 21:42:54 +0000

    Finish working and refactored palindrome method

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gemspec
 
 gem "rake", "~> 13.0"
 
-gem "minitest", "~> 5.0"
+gem 'minitest', '~> 5.8', '>= 5.8.4'
 
 gem "standard", "~> 1.3"
+
+gem "minitest-reporters", "~> 1.2", '>= 1.2.0'
--- lib/amatosich_palindrome.rb
+++ lib/amatosich_palindrome.rb
@@ -2,7 +2,26 @@
 
 require_relative "amatosich_palindrome/version"
 
-module AmatosichPalindrome
   class Error < StandardError; end
-  # Your code goes here...
-end
+  
+  class String
+  
+  # Returns true for a palindrome, false otherwise 
+  def palindrome? 
+    processed_content == processed_content.reverse
+  end 
+
+  # Returns the letters in the string. (interate thru letters, put in array, join at end)
+  def letters
+   self.chars.select { |c| c.match(/[a-z]/i) }.join
+  end 
+  
+    private
+    
+    # Returns content for palindrome testing.
+    def processed_content
+      self.letters.downcase
+    end 
+  end 
+
+
